131e346e135ca24141e190d13903a38b7d822a3a
Marwa Elali <marwaelali@gmail.com>
2022-03-06 18:34:41 -0500

    add tcl and sh scripts for experiment 3

---
+++ Experiment-3/exp3_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tcps=("Reno" "SACK")
+queue_algorithms=("DropTail" "RED")
+PREFIX=/course/cs4700f12/ns-allinone-2.35/bin/ns
+
+for variant in "${tcps[@]}"
+do
+    echo "Running simulation for $variant"
+
+    for queue in "${queue_algorithms[@]}"
+    do
+        echo "Simulating $variant with $queue algorithm"
+        $PREFIX experiment_3.tcl $variant $queue
+    done
+done
