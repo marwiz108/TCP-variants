ae486e640b7b5580328d9dbf93cd08bc0d1e243c
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-06 20:45:30 -0500

    Correcting errors from Alpha Test

--- Experiment-2/test_script.sh
+++ Experiment-2/test_script.sh
@@ -6,14 +6,12 @@ PREFIX=/course/cs4700f12/ns-allinone-2.35/bin/ns
 # Run simulation for Experiment 2 for 4 TCP variant pairs: Reno/Reno, NewReno/Reno, Vegas/Vegas, and Newreno/Vegas
 for variant_pair in "${TCP_VARIANT_PAIRS[@]}"
 do
-    echo "Running Simulation for 2 TCP Variants: $variant_pair"
 
     # Run simulation for each pair of TCP variants with CBR flow starting from 1Mbps till the bottleneck capacity
     for rate in {1..10}
     do
         for start_time in $(seq 0 0.5 2.0)
         do
-            echo "Simulating for 2 variants $variant_pair with CBR flow: $rate and TCP Start Time: $start_time"
             $PREFIX experiment_2.tcl $variant_pair $rate $start_time
 
         done
