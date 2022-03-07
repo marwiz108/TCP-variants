cb95ba12f22b5ee4d06e34361a390c4280ae4f07
marwiz108 <marwaelali@gmail.com>
2022-03-06 20:24:32 -0500

    fix typo error

--- Experiment-3/exp3_script.sh
+++ Experiment-3/exp3_script.sh
@@ -4,11 +4,9 @@ tcps=("Reno" "Sack1")
 queue_algorithms=("DropTail" "RED")
 PREFIX=/course/cs4700f12/ns-allinone-2.35/bin/ns
 
-for variant in "${tcps}"
+for variant in "${tcps[@]}"
 do
-    echo "Running simulation for $variant"
-
-    for queue in "${queue_algorithms}"
+    for queue in "${queue_algorithms[@]}"
     do
         echo "Simulating $variant with $queue algorithm"
         $PREFIX experiment_3.tcl $variant $queue
