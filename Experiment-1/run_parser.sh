2f947bebe0294e3d192d94beab6a4770dec11152
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-07 16:59:03 -0500

    Updates

--- Experiment-1/run_parser.sh
+++ Experiment-1/run_parser.sh
@@ -3,5 +3,9 @@
 file_path=$(pwd)
 
 for f in $file_path/trace_data_exp1/*
-do python3 parser.py exp1 $f res.csv
+do python3 parser.py exp1 $f res_exp1.csv
+done
+
+for f in $file_path/trace_data_exp2/*
+do python3 parser.py exp2 $f res_exp2.csv
 done
