00ba5307a2839d7620dc1dbb3d8954f63330867a
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-08 21:23:24 -0500

    Test run

--- Experiment-2/test_script.sh
+++ Experiment-2/test_script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-TCP_VARIANT_PAIRS=("Reno Reno" "Newreno Reno" "Vegas Vegas" "Newreno Vegas")
 PREFIX=/course/cs4700f12/ns-allinone-2.35/bin/ns
 
+# Run simulation for Experiment 2 for 4 TCP variant pairs: Reno/Reno, NewReno/Reno, Vegas/Vegas, and Newreno/Vegas
+# Run for each pair of TCP variants with CBR flow starting from 1Mbps till the bottleneck capacity
 for rate in {1..10}
     do
-        $PREFIX experiment_2.tcl $rate 2.0 4.0
-
+        $PREFIX experiment2_NR_R.tcl $rate 1.0 3.0
+        $PREFIX experiment2_NR_V.tcl $rate 1.0 3.0
+        $PREFIX experiment2_R_R.tcl $rate 1.0 3.0
+        $PREFIX experiment2_V_V.tcl $rate 1.0 3.0
+        
     done
 
 # # Run simulation for Experiment 2 for 4 TCP variant pairs: Reno/Reno, NewReno/Reno, Vegas/Vegas, and Newreno/Vegas
