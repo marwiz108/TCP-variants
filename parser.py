fb884f0bf700b00b7b9082cfdc74c5b13c0c00ae
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-09 11:25:12 -0500

    Cleaning up files, except for Exp3

--- parser.py
+++ parser.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 #import os
+from __future__ import division
 import sys
 import csv
-from typing import final
-
 # constant
 
 ################################################################################################################
@@ -72,7 +71,12 @@ def thoughput(trace, srcNode, sinkNode, fid):
     # convert units from byte to bits and Mb and finally divided by path latency
     size_pkt_recv = size_pkt_recv * 8
     size_pkt_recv = size_pkt_recv / ( 1024 * 1024 )
-    thoughput_res = size_pkt_recv / time_usage
+    
+    # No time usage: No packet sent
+    try:
+        thoughput_res = size_pkt_recv / time_usage
+    except:
+        thoughput_res = 0.0
     
     # return the final result
     return thoughput_res
@@ -96,8 +100,11 @@ def pktDrop(trace,srcNode,fid):
         if trace[i][4] == 'tcp' and trace[i][0] == '-' and trace[i][2] == srcNode and trace[i][7] == fid:
             pkt_sent += 1
     
-    # calculating the drop rate
-    drop_rate = float(pkt_drop) / float(pkt_sent)
+    # No packet sent: No packet sent
+    try:
+        drop_rate = float(pkt_drop) / float(pkt_sent)
+    except:
+        drop_rate = 0.0
     
     #print(pkt_drop, pkt_sent)
     
@@ -127,31 +134,50 @@ def EtoELatency(trace,srcNode,fid):
     
     sum_latency = 0
     num_rtt = 0
-    invalid_rtt = 0 # there might be invalid rtt if the packet is dropped ?
 
     # loop through the dic to get total time usage also count the number of invalid rtts
     for rtt in rtts:
         if rtts[rtt][1] != 0:
             delay = float(rtts[rtt][1]) - float(rtts[rtt][0])
             sum_latency = sum_latency + delay
-        else:
-            invalid_rtt += 1
 
-    num_rtt = len(rtts) - invalid_rtt
+    num_rtt = len(rtts)
 
-    latency = sum_latency / num_rtt
+    # No rtt: No packet sent
+    try:
+        latency = sum_latency / num_rtt
+    except:
+        latency = 0.0
 
     return latency
 
-def get_meta_trace(TRACE_FILE):
-    meta_data = TRACE_FILE.split('/')[-1].split('_')
-    # exp1_Newreno_10Mb.tr
-    # exp1_Newreno_9Mb.tr
-    tcp_variant = meta_data[1]
-    cbr_flow = meta_data[2]
-    cbr_flow = cbr_flow[: -5]
+def get_meta_trace(TRACE_FILE, option):
+    if option == 'exp1':
+        meta_data = TRACE_FILE.split('/')[-1].split('_')
+        # exp1_Newreno_10Mb.tr
+        # exp1_Newreno_9Mb.tr
+        tcp_variant = meta_data[1]
+        cbr_flow = meta_data[2]
+        cbr_flow = cbr_flow[: -5]
 
-    return tcp_variant, cbr_flow
+        return tcp_variant, cbr_flow
+    
+    elif option == 'exp2':
+        # exp2 Vegas Vegas 9Mb 4.0 1.0.tr
+        meta_data = TRACE_FILE.split('/')[-1].split('_')
+        tcp_variant1, tcp_variant2 = meta_data[1], meta_data[2]
+        cbr_flow = meta_data[3][: -2]
+        tcp1_st = meta_data[4]
+        tcp2_st = meta_data[5][: -3]
+
+        return tcp_variant1, tcp_variant2, cbr_flow, tcp1_st, tcp2_st
+    
+    elif option == 'exp3':
+        meta_data = TRACE_FILE.split('/')[-1].split('_')
+        tcp_variant = meta_data[1]
+        queue_algo = meta_data[2][:-3]
+        print(tcp_variant, queue_algo)
+        return tcp_variant, queue_algo
 
 
 ###########################################################################################################
@@ -169,7 +195,6 @@ def main():
 
     # if the trace files are result for experiment 1
     if OPTION == "exp1":
-        
         # drop_rate (trace_list, source node, flow id)
         drop_rate = pktDrop(trace,'0','2')
         # thoughput(trace_list, source node, sink node, flow id)
@@ -180,12 +205,12 @@ def main():
         # write the data into csv file
         with open(RES_FILE, 'a+', newline='') as file:
             writer = csv.writer(file)
-            tcp_variant, cbr_flow = get_meta_trace(TRACE_FILE)
+            tcp_variant, cbr_flow = get_meta_trace(TRACE_FILE, 'exp1')
             writer.writerow([tcp_variant, cbr_flow, thoughput_res, drop_rate, latency])
 
     elif OPTION == "exp2":
-        # first TCP (source node = 0, sink node = 3, flow id = 2)
-        # second TCP (source node = 4, sink node = 5, flow id = 3)
+        # first TCP (source node = 0, flow id = 2)
+        # second TCP (source node = 4, flow id = 3)
         drop_rate1 = pktDrop(trace,'0','2')
         drop_rate2 = pktDrop(trace,'4','3')
 
@@ -198,42 +223,102 @@ def main():
         # write the data into csv file
         with open(RES_FILE, 'a+', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([TRACE_FILE,thoughput_res1,thoughput_res2,drop_rate1,drop_rate2,latency1,latency2])
+            tcp_variant1, tcp_variant2, cbr_flow, tcp1_st, tcp2_st = get_meta_trace(TRACE_FILE, 'exp2')
+            writer.writerow([tcp_variant1, thoughput_res1, drop_rate1, latency1, tcp_variant2, thoughput_res2, drop_rate2, latency2, cbr_flow])
 
     elif OPTION == "exp3":
         # for exp3 we only care about latency and throughput verse time
         rtts = {}
         tp_sec = {} # key = sec value = throughput at the time
         # overall end to end latency
-        EtoELatency(trace,'0', '2')
+        #EtoELatency(trace,'0', '2')
 
         for i in range(len(trace)):
             # event == recv
-            if trace[i][0] == 'r':
+            #if trace[i][0] == 'r':
+            if trace[i][7] == '2':
                 # flow id
-                if trace[i][7] == '2':
+                if trace[i][0] == 'r':
                     # ack recv at source node
                     if trace[i][4] == 'ack' and trace[i][3] == '0':
-                        tp_sec[trace[i][1]] += trace[i][5]
-                    # tcp rece at sink node
-                    elif trace[i][4] == 'tcp' and trace[i][3] == '3':
-                        tp_sec[trace[i][1]] += trace[i][5]
-                
-            # calculating... per 1 second?
-            final_dic = {}
-            for sec in tp_sec:
-                # count the time (time interval)
-                count = 1
-                if sec <= count:
-                    thoughput_at_sec = tp_sec[sec] * 8 / (1024*1024) / 1.0
-                    final_dic[count] += thoughput_at_sec
-                else:
-                    count += 1
+                        # tp_sec[time] = tp_sec[time] + size(in bytes)
+                        try:
+                            tp_sec[trace[i][1]] += trace[i][5]
+                        except:
+                            tp_sec[trace[i][1]] = trace[i][5]
+                        
+                        try:
+                            rtts[trace[i][10]] = [0, float(trace[i][1])]
+                        except:
+                            rtts[trace[i][10]] = [0, 0]
+                            
 
-            with open(RES_FILE, 'a+', newline='') as file:
-                for sec in final_dic:
-                    writer = csv.writer(file)
-                    writer.writerow([sec, final_dic[sec]])
+                    # tcp recv at sink node
+                    elif trace[i][4] == 'tcp' and trace[i][3] == '3':
+                        try:
+                            tp_sec[trace[i][1]] += trace[i][5]
+                        except:
+                            tp_sec[trace[i][1]] = trace[i][5]
+                
+                elif trace[i][0] == '+':
+                    if trace[i][2] == '0' and trace[i][4] == 'tcp' :
+                        try:
+                            rtts[trace[i][10]] = [float(trace[i][1]), 0]
+                        except:
+                            rtts[trace[i][10]] = [0, 0]
+                
+        # calculating... per 1 second?
+        final_dic = {}
+        latency_res = {}
+        count = 1
+
+        for sec in tp_sec:
+            # count the time (time interval)
+            if float(sec) <= count:
+                thoughput_at_sec = (float(tp_sec[sec]) * 8 / (1024*1024) / 1.0)
+                #print(thoughput_at_sec)
+                try:
+                    final_dic[count] += thoughput_at_sec
+                except:
+                    final_dic[count] = thoughput_at_sec
+            elif float(sec) > count:
+                count += 1
+        cnt = 1
+        num_pkt = 0
+        for rtt in rtts:
+            time_stamp = rtts[rtt][1]
+            if time_stamp <= cnt:
+                delay = (float(rtts[rtt][1]) - float(rtts[rtt][0]))
+                num_pkt += 1
+                #print(delay, num_pkt)
+                try:
+                    latency_res[cnt][0] += delay
+                    latency_res[cnt][1] = num_pkt
+                except:
+                    latency_res[cnt] = [delay,num_pkt]
+            else:
+                cnt += 1
+                
+
+        #for time in final_dic:
+        #    print(str(time) + " = " + str(final_dic[time]) )
+        #for time in latency_res:
+            #print(str(time) + " = " + str(latency_res[time]) )
+        
+        with open(RES_FILE, 'a+', newline='') as file:
+            for sec in final_dic:
+                writer = csv.writer(file)
+                tcp_variant, queue_algo = get_meta_trace(TRACE_FILE, 'exp3')
+                writer.writerow(['TP', tcp_variant, queue_algo, sec, final_dic[sec]])
+            for sec in latency_res:
+                writer = csv.writer(file)
+                tcp_variant, queue_algo = get_meta_trace(TRACE_FILE, 'exp3')
+                try:
+                    res = (float(latency_res[sec][0]) / float(latency_res[sec][1]))
+                except:
+                    res = -1
+                writer.writerow(['LAT', tcp_variant, queue_algo, sec, res])
+
     else:
         print("wrong option for parser")
 
