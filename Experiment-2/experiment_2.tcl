d201b642bf97709c091b34fc0364bb47fce8960d
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-08 20:24:55 -0500

    New Test runs

--- Experiment-2/experiment_2.tcl
+++ Experiment-2/experiment_2.tcl
@@ -1,27 +1,23 @@
 set ns [new Simulator]
 
 # Retrieve simulation parameters from shell command line
-# 1. The pair of TCP Variants
-set tcp_variant1 [lindex $argv 0]
-set tcp_variant2 [lindex $argv 1]
-
 # 2. The CBR flow
-set cbr_flow [lindex $argv 2]mb
+set cbr_flow [lindex $argv 0]mb
 
 # 3. The TCP variant pair start times
-set tcp1_start_time [lindex $argv 3]
-set tcp2_start_time [lindex $argv 4]
+set tcp1_start_time [lindex $argv 1]
+set tcp2_start_time [lindex $argv 2]
 
 # Setup the output file name
 set trace_file_name exp2_
 # TCP/... TCP/...
-append trace_file_name $tcp_variant1 _ $tcp_variant2
+append trace_file_name "Newreno" _ "Reno"
 append trace_file_name _$cbr_flow
 append trace_file_name _$tcp1_start_time
 append trace_file_name _$tcp2_start_time.tr
 
 # Console log message
-puts "$trace_file_name || Running Sim for TCP 1: $tcp_variant1 | TCP 2: $tcp_variant2 | CBR: $cbr_flow | TCP 1 ST: $tcp1_start_time | TCP 2 ST: $tcp2_start_time"
+puts "$trace_file_name || Running Sim for TCP 1: Newreno | TCP 2: Reno | CBR: $cbr_flow | TCP 1 ST: $tcp1_start_time | TCP 2 ST: $tcp2_start_time"
 
 # Open simulation trace file
 set nf [open trace_data/$trace_file_name w]
@@ -49,102 +45,93 @@ proc finish {} {
 #
 
 # Setting up 6 nodes as part of the network blueprint
-# TCP Source 1
 set N1 [$ns node]
-# TCP Source 2
-set N5 [$ns node]
-# CBR Source - Bottleneck Cap: 10Mbps
 set N2 [$ns node]
-# UDP Sink - Null
 set N3 [$ns node]
-# TCP Sink 1
 set N4 [$ns node]
-# TCP Sink 2
+set N5 [$ns node]
 set N6 [$ns node]
 
 # Create network links. Default queueing mechanism (Droptail)
 $ns duplex-link $N1 $N2 10Mb 12ms DropTail
-$ns duplex-link $N5 $N2 10Mb 12ms DropTail
 $ns duplex-link $N2 $N3 10Mb 12ms DropTail
 $ns duplex-link $N3 $N4 10Mb 12ms DropTail
+$ns duplex-link $N2 $N5 10Mb 12ms DropTail
 $ns duplex-link $N3 $N6 10Mb 12ms DropTail
 
 # Set queue limit between nodes N2 and N3
-$ns queue-limit $N1 $N2 50
-$ns queue-limit $N5 $N2 50
 $ns queue-limit $N2 $N3 50
-$ns queue-limit $N3 $N4 50
-$ns queue-limit $N3 $N6 50
 
 # UDP-CBR Connection
 # Setup a UDP connection for CBR flow at N2
 set udp [new Agent/UDP]
 $ns attach-agent $N2 $udp
 
-# Setup CBR over UDP at N2
-set cbr_stream [new Application/Traffic/CBR]
-$cbr_stream set rate_ $cbr_flow
-$cbr_stream set type_ CBR
-$cbr_stream set random_ false
-$cbr_stream attach-agent $udp
-
 # Setup Sink at N3
-set cbr_sink [new Agent/Null]
-$ns attach-agent $N3 $cbr_sink
+set null [new Agent/Null]
+$ns attach-agent $N3 $null
+
+# Setup CBR over UDP at N2
+set cbr [new Application/Traffic/CBR]
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set random_ false
+$cbr set rate_ $cbr_flow
+$cbr attach-agent $udp
 
 # Connection: UDP - From N2 to N3
-$ns connect $udp $cbr_sink
+$ns connect $udp $null
 $udp set fid_ 1
+
+
 
 # TCP-FTP Connection 1
 # Setup the first TCP connection from N1 to N4
-set tcp_var1 [new Agent/TCP/$tcp_variant1]
-$tcp_var1 set window_ 100
+set tcp_var1 [new Agent/TCP/Newreno]
 $ns attach-agent $N1 $tcp_var1
 
 # Setup FTP application at N1 for data stream
-set ftp_stream_var1 [new Application/FTP]
-$ftp_stream_var1 set type_ FTP
-$ftp_stream_var1 attach-agent $tcp_var1
+set ftp_stream1 [new Application/FTP]
+$ftp_stream1 set type_ FTP
+$ftp_stream1 attach-agent $tcp_var1
 
 # Setup TCP Sink at N4
-set tcp_sink_var1 [new Agent/TCPSink]
-$ns attach-agent $N4 $tcp_sink_var1
+set tcp_sink1 [new Agent/TCPSink]
+$ns attach-agent $N4 $tcp_sink1
 
 # Connection: TCP 1 - From N1 to N4
-$ns connect $tcp_var1 $tcp_sink_var1
+$ns connect $tcp_var1 $tcp_sink1
 $tcp_var1 set fid_ 2
 
 # TCP-FTP Connection 2
 # Setup the second TCP connection from N5 to N6
-set tcp_var2 [new Agent/TCP/$tcp_variant2]
-$tcp_var2 set window_ 100
+set tcp_var2 [new Agent/TCP/Reno]
 $ns attach-agent $N5 $tcp_var2
 
 # Setup FTP application at N5 for data stream
-set ftp_stream_var2 [new Application/FTP]
-$ftp_stream_var2 set type_ FTP
-$ftp_stream_var2 attach-agent $tcp_var2
+set ftp_stream2 [new Application/FTP]
+$ftp_stream2 set type_ FTP
+$ftp_stream2 attach-agent $tcp_var2
 
 # Setup TCP Sink at N6
-set tcp_sink_var2 [new Agent/TCPSink]
-$ns attach-agent $N6 $tcp_sink_var2
+set tcp_sink2 [new Agent/TCPSink]
+$ns attach-agent $N6 $tcp_sink2
 
 # TCP 2 - From N5 to N6
-$ns connect $tcp_var2 $tcp_sink_var2
+$ns connect $tcp_var2 $tcp_sink2
 $tcp_var2 set fid_ 3
 
 # Event schedule for TCP and UDP connections
 # Starting CBR at 0.0s
-$ns at 0.1 "$cbr_stream start"
+$ns at 0.1 "$cbr start"
 # Starting TCP variant pairs after CBR starts (stabalization check)
-$ns at $tcp1_start_time "$ftp_stream_var1 start"
-$ns at $tcp2_start_time "$ftp_stream_var2 start"
+$ns at $tcp1_start_time "$ftp_stream1 start"
+$ns at $tcp2_start_time "$ftp_stream2 start"
 
-$ns at 12.1 "$cbr_stream stop"
-$ns at 12.0 "$ftp_stream_var1 stop"
-$ns at 12.0 "$ftp_stream_var2 stop"
+$ns at 12.0 "$cbr stop"
+$ns at 12.0 "$ftp_stream1 stop"
+$ns at 12.0 "$ftp_stream2 stop"
 
 # Run simulation
-$ns at 12.5 "finish"
+$ns at 12.1 "finish"
 $ns run
--- Experiment-2/test_script.sh
+++ Experiment-2/test_script.sh
@@ -3,17 +3,23 @@
 TCP_VARIANT_PAIRS=("Reno Reno" "Newreno Reno" "Vegas Vegas" "Newreno Vegas")
 PREFIX=/course/cs4700f12/ns-allinone-2.35/bin/ns
 
-# Run simulation for Experiment 2 for 4 TCP variant pairs: Reno/Reno, NewReno/Reno, Vegas/Vegas, and Newreno/Vegas
-for variant_pair in "${TCP_VARIANT_PAIRS[@]}"
-do
-    # Run simulation for each pair of TCP variants with CBR flow starting from 1Mbps till the bottleneck capacity
-    for rate in {1..10}
+for rate in {1..10}
     do
-        $PREFIX experiment_2.tcl $variant_pair $rate 2.0 5.0
-        $PREFIX experiment_2.tcl $variant_pair $rate 4.0 4.0
+        $PREFIX experiment_2.tcl $rate 2.0 4.0
 
     done
-done
+
+# # Run simulation for Experiment 2 for 4 TCP variant pairs: Reno/Reno, NewReno/Reno, Vegas/Vegas, and Newreno/Vegas
+# for variant_pair in "${TCP_VARIANT_PAIRS[@]}"
+# do
+#     # Run simulation for each pair of TCP variants with CBR flow starting from 1Mbps till the bottleneck capacity
+#     for rate in {1..10}
+#     do
+#         $PREFIX experiment_2.tcl $variant_pair $rate 2.0 5.0
+#         $PREFIX experiment_2.tcl $variant_pair $rate 4.0 4.0
+
+#     done
+# done
 
 # REFERENCES:
 # https://www.isi.edu/nsnam/ns/doc/node387.html TCP Agents
