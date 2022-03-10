7c21f1bfad5c9ac7e11155d713011a6309dd87a5
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-03-09 19:01:03 -0500

    File finalization

--- run_parser.sh
+++ run_parser.sh
@@ -7,7 +7,6 @@ for f in $file_path/Experiment-1/trace_data/*
         python3 parser.py exp1 $f res_exp1.csv
     done
 
-
 for f in $file_path/Experiment-2/trace_data/*
     do 
         python3 parser.py exp2 $f res_exp2.csv
